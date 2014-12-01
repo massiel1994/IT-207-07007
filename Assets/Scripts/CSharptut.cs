92e34b4c7fc2b10965c788c2459a37dca0652f1d
unknown <massiel.baez@ACC-GAME-20.lab.snhu.edu>
2014-12-01 13:23:50 -0500

    Hello

---
+++ Assets/Scripts/CSharptut.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+public class CSharptut : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Variables myClass;
+		myClass = new Variables ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+public class Variables
+{
+	public void Test()
+	{
+				int x = 100;
+				int y;
+				y = 150;
+
+				int z;
+
+				z = x + y;
+
+				Debug.Log( String.Format( "{0}", z) );
+			
+		string Name = "Joe";
+		string Surname = "Schmoe";
+
+		Debug.Log(Name + " " + Surname);
+		Debug.Log (String.Concat (Name, " ", Surname));
+		}
+	}
